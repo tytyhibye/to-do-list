335a87e932b32d68d6c4f7be84f99eb948ee133b
Tyler Bates <Fondlytyler@gmail.com>
2020-05-21 08:02:58 -0700

    refactor code to reflect object within objects.

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -22,12 +22,6 @@ namespace ToDoList.Controllers
       return View();
     }
 
-    [HttpPost("/categories")]
-    public ActionResult Create(string categoryName)
-    {
-      Category newCategory = new Category(categoryName);
-      return RedirectToAction("Index");
-    }
 
     [HttpGet("/categories/{id}")]
     public ActionResult Show(int id)
@@ -38,6 +32,19 @@ namespace ToDoList.Controllers
       model.Add("category", selectedCategory); // These will be stored with the keys "category" and "items".
       model.Add("items", categoryItems);
       return View(model); // The Dictionary, which is named model, will be passed into View().
+    }
+    // This one creates new Items within a given Category, not new Categories:
+    [HttpPost("/categories/{categoryId}/items")]
+    public ActionResult Create(int categoryId, string itemDescription) // The method now takes two arguments: the categoryId we passed into a hidden form field and an itemDescription that contains the user's form input.
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category foundCategory = Category.Find(categoryId); // Using the categoryId provided as an argument, we locate the Category object our new Item should belong to and call it foundCategory.
+      Item newItem = new Item(itemDescription); // We then create a new Item object with the user's form input.
+      foundCategory.AddItem(newItem); // We add the newItem to the foundCategory with our existing AddItem() method.
+      List<Item> categoryItems = foundCategory.Items; // We retrieve all other Items that correspond to this category and add it to our model. We do this because the view we'll render at the end of this route requires this information.
+      model.Add("items", categoryItems);
+      model.Add("category", foundCategory); // We also add the foundCategory to our model.
+      return View("Show", model); // Finally, we pass in our model data to View(), instructing it to render the Category detail page, which is the Show.cshtml view.
     }
   }
 }
--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -6,25 +6,23 @@ namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-
-    [HttpGet("/items")]
-    public ActionResult Index()
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
     {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
     }
 
-    [HttpGet("/items/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
 
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
     {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
+      Category category = Category.Find(categoryId);
+      return View(category);
     }
 
     [HttpPost("/items/delete")]
